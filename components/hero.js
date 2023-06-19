77836f6350b68debd35791fea5323a52d74254e7
Lukman10a <abdulrauflukman9@gmail.com>
2023-06-19 10:08:09 +0100

    made some changes

--- components/hero.js
+++ components/hero.js
@@ -5,9 +5,12 @@ import hero_image from "../public/hero_image.jpg";
 const Hero = () => {
   return (
     <div className="relative flex justify-center items-center text-white ">
+      <div className="bg-yellow-950 opacity-60 absolute w-full h-full"></div>
       <div className="absolute flex flex-col justify-center items-center gap-4">
         <p className="text-3xl lg:text-7xl font-bold">My Portfolio</p>
-        <p>Crafting Dynamic Web Experiences That Drive Results</p>
+        <p className="lg:text-2xl">
+          Crafting Dynamic Web Experiences That Drive Results
+        </p>
       </div>
       <Image src={hero_image} />
     </div>
--- components/navBar.js
+++ components/navBar.js
@@ -2,34 +2,15 @@
 
 const NavBar = () => {
   return (
-    // <nav className="grid grid-rows-2 text-white cursor-pointer hover:text-blue-300">
-    //   <div className="bg-slate-800 flex gap-4 justify-around items-center">
-    //     <p>Email: abdulrauflukman9@gmail.com</p>
-    //     <p>Phone No: +2348089287065</p>
-    //   </div>
-    //   <div className="flex justify-around items-center gap-10 p-5 bg-lime-900">
-    //     <div>Lukman</div>
-    //     <ul className="flex space-x-3">
-    //       <li>Home</li>
-    //       <li>About</li>
-    //       <li>Portfolio</li>
-    //       <li>Services</li>
-    //       <li>Contact</li>
-    //     </ul>
-    //   </div>
-    // </nav>
-
-    // ...
-
     <nav className="bg-gray-900 text-white">
       <div className="flex items-center p-4 gap-5">
         <p>Email: abdulrauflukman9@gmail.com</p>
         <p>Phone No: +2348089287065</p>
       </div>
-      <div className="bg-lime-900 flex justify-between items-center p-4">
-        <div className="text-2xl font-bold">Your Name</div>
+      <div className="bg-lime-900 flex justify-evenly items-center gap-20 p-4">
+        <div className="text-2xl font-bold cursor-pointer">Your Name</div>
         <div className="flex items-center space-x-4">
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-4 cursor-pointer">
             <li>
               <a className="hover:text-blue-300">Home</a>
             </li>
