c9f5d7dc6e66aebc87a291fc8f3c0ad7c9082c5f
Lukman10a <abdulrauflukman9@gmail.com>
2023-06-19 09:32:35 +0100

    made some changes

--- components/hero.js
+++ components/hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className="relative flex justify-center items-center text-white ">
       <div className="absolute flex flex-col justify-center items-center gap-4">
-        <p className="text-7xl font-bold">My Portfolio</p>
+        <p className="text-3xl lg:text-7xl font-bold">My Portfolio</p>
         <p>Crafting Dynamic Web Experiences That Drive Results</p>
       </div>
       <Image src={hero_image} />
